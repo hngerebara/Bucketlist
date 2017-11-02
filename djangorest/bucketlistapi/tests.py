7eb9b0aa05e07921e2bc5367243f53b4e76dab1e
Hope Ngerebara <hope.ngerebara@andela.com>
2017-11-02 17:01:41 +0100

    Create a new bucketlist with tests

--- djangorest/bucketlistapi/tests.py
+++ djangorest/bucketlistapi/tests.py
@@ -2,6 +2,9 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.core.urlresolvers import reverse
 from .models import Bucketlist
 
 # Create your tests here.
@@ -20,3 +23,19 @@ class ModelTestCase(TestCase):
         self.bucketlist.save()
         new_count = Bucketlist.objects.count()
         self.assertNotEqual(old_count, new_count)
+
+class ViewTestCase(TestCase):
+    """ Test suite for views"""
+
+    def setUp(self):
+        """Define the test client and other test variables"""
+        self.client = APIClient()
+        self.bucketlist_data = {'name': 'Go to Port Harcourt'}
+        self.response = self.client.post(
+            reverse('Create'),
+            self.bucketlist_data,
+            format="json")
+
+    def test_api_can_create_a_bucketlist(self):
+        """Test that a bucket has been created"""
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
--- djangorest/bucketlistapi/views.py
+++ djangorest/bucketlistapi/views.py
@@ -2,5 +2,16 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import BucketlistSerializer
+from .models import Bucketlist
+
 
 # Create your views here.
+class CreateView(generics.ListCreateAPIView):
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
+
+    def perform_create(self, serializer):
+        """Saves the post date when a new bucketlist is created"""
+        serializer.save()
