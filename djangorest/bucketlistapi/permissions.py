4e1fb8bb2cad8a433dd2658987c4f119e43313d1
Hope Ngerebara <hope.ngerebara@andela.com>
2017-11-29 14:52:30 +0100

    remove unused code

--- djangorest/bucketlistapi/permissions.py
+++ djangorest/bucketlistapi/permissions.py
@@ -1,15 +1,3 @@
-# from rest_framework.permissions import BasePermission
-# from .models import Bucket, Bucketlist
-#
-# class IsOwner(BasePermission):
-#     """Custom permission class to allow only bucketlist owners to edit them."""
-#
-#     def has_object_permission(self, request, view, obj):
-#         """Return True if permission is granted to the bucketlist owner."""
-#         if isinstance(obj, Bucket):
-#             return obj.owner == request.user
-#         return obj.owner == request.user
-
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAdminOrReadOnly(BasePermission):
